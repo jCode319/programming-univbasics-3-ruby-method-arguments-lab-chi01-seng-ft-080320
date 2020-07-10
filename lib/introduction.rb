426fa836f94bf0339db74dfbdb47d69ebdd0aaba
Jarrell Jackson <jdjack3@gmail.com>
2020-07-10 00:25:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,7 +14,11 @@ end
 
 introduction_with_language("name", "language")
 
+def introduction_with_language_optional (name, language = 'Ruby')
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("name", "language")
 
 
 
